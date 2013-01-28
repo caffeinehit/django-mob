49a1718e0b3c39a2786858d99c3f2ab3c86059ff
Alen Mujezinovic <flashingpumpkin@gmail.com>
2013-01-28 15:53:36 +0000

    Initial commit

---
+++ tests/settings.py
@@ -0,0 +1,32 @@
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Tester', 'test@example.com'),
+)    
+
+SECRET_KEY = 'secret'
+
+MEDIA_ROOT = os.path.join(
+    os.path.dirname(__file__),
+    'test-media')
+
+MEDIA_URL = '/test-media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    },
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'mob',
+    'tests.app',
+]
+
+
